fbc060e3e0bd1b3c7c7e3f7f5287102a7031a852
Andres J. Mayorga <httpmayorga@gmail.com>
2023-11-22 20:38:52 -0500

    Cambio # en codigo

--- EjemploGraficas.py
+++ EjemploGraficas.py
@@ -1,8 +1,7 @@
+#Andres Mayorga
+#Ejemplo Graficas
 import numpy as np
 import matplotlib.pyplot as plt
-
-## Programa que toma datos aleatorios para así generar la grafica necesaria
-## Para ver la grafica, solo se debe desmarcar el bloque de codigo que se desee
 rng = np.random.default_rng(42)
 
 # Datos de calificaciones de los estudiantes
@@ -47,8 +46,6 @@ plt.show()
 """
 
 #Histograma
-
-
 
 plt.hist(matematicas, bins=10)
 plt.title('Distribucion de las calificaciones de matematicas')
